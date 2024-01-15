f5c15bb14f0aef0df60785d88948ff9ddee76ddf
Jeremy Rapich <jeremysr@protonmail.com>
2024-01-14 17:48:28 -0700

    commenting out links from boilerplate that are not yet used

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,29 +3,30 @@ import { createRoot } from "react-dom/client";
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
 import App from "./App.jsx";
+import { Home } from "./pages/index.js";
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <App />,
-    errorElement: <App error={<Error />} />,
+    // errorElement: <App error={<Error />} />,
     children: [
       {
         index: true,
         element: <Home />,
       },
-      {
-        path: "/login",
-        element: <Login signup={false} />,
-      },
-      {
-        path: "/signup",
-        element: <Login signup={true} />,
-      },
-      {
-        path: "/logout",
-        element: <Logout />,
-      },
+      // {
+      //   path: "/login",
+      //   element: <Login signup={false} />,
+      // },
+      // {
+      //   path: "/signup",
+      //   element: <Login signup={true} />,
+      // },
+      // {
+      //   path: "/logout",
+      //   element: <Logout />,
+      // },
     ],
   },
 ]);
